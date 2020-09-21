fd70588046ce8c0da33e51619f1cc31bac7887a0
Kevin Blount <kvbchamp@gmail.com>
2020-09-21 13:32:25 -0400

    rename modelname to Folder

--- mptt_app/models.py
+++ mptt_app/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from mptt.models import MPTTModel, TreeForeignKey
 
-class Genre(MPTTModel):
+class Folder(MPTTModel):
     name = models.CharField(max_length=200, unique=True)
     parent = TreeForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='children')
 
