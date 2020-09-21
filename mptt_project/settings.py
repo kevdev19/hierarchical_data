38f60510dfc22d8e94550cd3578596d0a7f4ccaa
Kevin Blount <kvbchamp@gmail.com>
2020-09-21 12:57:06 -0400

    added django-mptt using poetry add

--- mptt_project/settings.py
+++ mptt_project/settings.py
@@ -37,9 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'mptt_app',
     'mptt',
-
+    'mptt_app',
 ]
 
 MIDDLEWARE = [
